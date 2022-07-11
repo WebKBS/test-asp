95a823a4dd0c39883a079492e018461417a5cb8f
kang <kbs21c1987@gmail.com>
2022-07-11 13:58:55 +0900

    add

--- sw.js
+++ sw.js
@@ -79,9 +79,9 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        alert.log('설치가 완료되었습니다. 브라우저가 종료됩니다.');
       } else {
-        console.log('User dismissed the A2HS prompt');
+        alert.log('설치 실패하였습니다.');
       }
       deferredPrompt = null;
     });
