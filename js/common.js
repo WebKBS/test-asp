dee1b173b81fd7b235f314b9a7ee984e6a44574d
kang <kbs21c1987@gmail.com>
2022-10-26 17:57:06 +0900

    수정완료

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ const modal = document.querySelector('.modal');
 const modalBackground = document.querySelector('.modal .background');
 const mintingButton = document.querySelector('.link_wrap button');
 const connectButton = document.querySelector('.connect_btn');
+const klayButton = document.querySelector('.klay_btn');
 
 connectButton.addEventListener('click', function () {
   // 모달 오픈시 배경 블러
@@ -11,9 +12,12 @@ connectButton.addEventListener('click', function () {
   // 모달 ON
   modal.classList.add('on');
 
-  // 버튼 클릭시 ON
-  connectButton.querySelector('.state_text').textContent = 'ON';
-  connectButton.classList.add('on');
+  // 버튼 활성화 라면 텍스트 변경
+  if (connectButton.classList.contains('on')) {
+    connectButton.querySelector('.state_text').textContent = 'ON';
+  } else {
+    connectButton.querySelector('.state_text').textContent = 'OFF';
+  }
 });
 
 /* 모달 해제 이벤트 */
@@ -23,8 +27,14 @@ modalBackground.addEventListener('click', function () {
 
   //모달 닫기
   modal.classList.remove('on');
-
-  // 버튼 클릭시 OFF
-  connectButton.querySelector('.state_text').textContent = 'OFF';
-  connectButton.classList.remove('on');
 });
+
+klayButton.addEventListener('click', connectOnOff);
+
+// 클레이튼 클릭시 connectButton On
+function connectOnOff() {
+  this.closest('.modal').classList.remove('on');
+  connectButton.querySelector('.state_text').textContent = 'ON';
+  connectButton.classList.add('on');
+  container.classList.remove('on');
+}
