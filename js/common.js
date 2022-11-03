2df92521058cb627b10abc36d9aa52613b326b4f
강범수 <kbs21c1987@gamil.com>
2022-11-03 13:46:10 +0900

    add

--- js/common.js
+++ js/common.js
@@ -1,39 +1,18 @@
-const container = document.querySelector('.container');
-const modal = document.querySelector('.modal');
-const modalBackground = document.querySelector('.modal .background');
-const connectButton = document.querySelector('.connect_btn');
-const klayButton = document.querySelector('.klay_btn');
+const sideOpenBtn = document.querySelector('.side_open_btn');
+const sideCloseBtn = document.querySelector('.side_close_btn');
+const sideMenu = document.querySelector('.side_menu');
 
-connectButton.addEventListener('click', function () {
-  // 모달 오픈시 배경 블러
-  container.classList.add('on');
+sideOpenBtn.addEventListener('click', openSideMenu);
+sideCloseBtn.addEventListener('click', closeSideMenu);
 
-  // 모달 ON
-  modal.classList.add('on');
+function openSideMenu() {
+  sideMenu.classList.add('on');
+  document.body.classList.add('on');
+  document.querySelector('html').classList.add('on');
+}
 
-  // 버튼 활성화 라면 텍스트 변경
-  if (connectButton.classList.contains('on')) {
-    connectButton.querySelector('.state_text').textContent = 'ON';
-  } else {
-    connectButton.querySelector('.state_text').textContent = 'OFF';
-  }
-});
-
-/* 모달 해제 이벤트 */
-modalBackground.addEventListener('click', function () {
-  //모달 해지시 블러 삭제
-  container.classList.remove('on');
-
-  //모달 닫기
-  modal.classList.remove('on');
-});
-
-klayButton.addEventListener('click', connectOnOff);
-
-// 클레이튼 클릭시 connectButton On
-function connectOnOff() {
-  this.closest('.modal').classList.remove('on');
-  connectButton.querySelector('.state_text').textContent = 'ON';
-  connectButton.classList.add('on');
-  container.classList.remove('on');
+function closeSideMenu() {
+  sideMenu.classList.remove('on');
+  document.body.classList.remove('on');
+  document.querySelector('html').classList.remove('on');
 }
